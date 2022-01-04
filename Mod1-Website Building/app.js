a0b4d8f64243e9f92927c9f37577f0338355c32c
abelgobran <74524485+abelgobran@users.noreply.github.com>
2022-01-04 16:22:16 -0500

    css,html,js changes.
    corrected errors in js hindering poster injection
    changes to template

--- Mod1-Website Building/app.js
+++ Mod1-Website Building/app.js
@@ -1,20 +1,27 @@
 
-
 const image = document.getElementById("image")
-const inputInfo = document.getElementById("search")
-let info ='endgame'
-const movie = `https://movie-database-imdb-alternative.p.rapidapi.com/?s=${info}&r=json&page=1`
+const inputForm = document.getElementById('form')
+let inputInfo = document.getElementById('search')
+const display = document.getElementById('display')
 
 
-callAPI(movie)
 
-function callAPI(movie_url){
+const movie = 'https://movie-database-imdb-alternative.p.rapidapi.com/?s='
+
+const link = '&r=json&page=1'
+
+
+// callAPI(movie)
+
+
+function callAPI(movie_url) {
+
     fetch(movie_url, {
         "method": "GET",
-                        "headers": {
-                            "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
-                            "x-rapidapi-key": "825986eacfmshbc25a9323ddf210p13a23ajsnbc487986ec1a"
-                        }
+        "headers": {
+            "x-rapidapi-host": "movie-database-imdb-alternative.p.rapidapi.com",
+            "x-rapidapi-key": "825986eacfmshbc25a9323ddf210p13a23ajsnbc487986ec1a"
+        }
     })
         .then((response) => response.json())
         .then((movieInfo) => {
@@ -24,37 +31,51 @@ function callAPI(movie_url){
         })
         .catch(error => {
             console.error(error);
+            alert(error);
         });
 
-    }
+}
 
 
+function displayResults(movieInfo) {
+    display.innerHTML = ''
 
- function displayResults(movieInfo){
+    movieInfo.forEach(movieInfo => {
+        const { Poster, Title, Year, Type } = movieInfo
 
-   
-     movieInfo.forEach(movieInfo => {
-        const {Title, Year, Type, Poster} = movieInfo
+        console.log(Poster);
         const displayMovie = document.createElement('div')
-        displayMovie.classList.add('movieInfo')
-            displayMovie.innerHTML = `<img src=${Poster}>
+        displayMovie.classList.add('movie')
+        displayMovie.innerHTML = ` <div class="movie">
+            <img src='${Poster}'>
+            
+            <div id="title">${Title}</div>
+            <div id ="type">${Type}</div>
+          
+            <div id="year">${Year}</div>
+        </div> `
 
-                                        <div> 
-                                        <h2>${Title}</H2>
-                                        <p>${Year}</p>
-                                        <p>${Type}</p>
-                                        
-                                        </div>
-                                        
-                                        `
-        
+        display.appendChild(displayMovie)
+
     });
 }
 
-inputInfo.addEventListener("submit", (e) =>{ 
-    
-    console.log(inputInfo);
-    // e.preventfault()
-    const searchInfo = search.value
-    displayResults(searchInfo)
+inputForm.addEventListener("submit", (e) => {
+
+    e.preventDefault()
+
+    // console.log(inputForm);
+    const searchResult = inputInfo.value
+    if (searchResult) {
+        console.log(searchResult);
+
+        callAPI(movie + searchResult + link)
+    } else
+        alert('Please enter a  field into search')
+
+
+
 })
+
+
+
